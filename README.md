ac8204dcc16c068601737ed79f8a9fe328550f0e
ckcollab <eric@ckcollab.com>
2014-05-04 02:01:35 -0700

    adding project

---
+++ README.md
@@ -0,0 +1,37 @@
+django-pin-password
+===================
+
+This is a simple app that adds a site-wide pin-passcode for quick authentication. I wrote this originally for my personal
+motivation tracker called [chin up](https://github.com/ckcollab/chin-up) so I could quickly login from my phone.
+
+You enter a PIN passcode (using 0-9 and #) and when you have entered the correct pin you will be logged in as `PIN_PASSCODE_USERNAME`
+and forwarded either the index or the page you were trying to request.
+
+
+Installation
+============
+
+Django Settings:
+
+```python
+INSTALLED_APPS += (
+    'pin_passcode',
+)
+```
+
+```python
+MIDDLEWARE_CLASSES += (
+    'pin_passcode.middleware.PinPasscodeMiddleware',
+)
+```
+
+```python
+PIN_PASSCODE_USERNAME = 'eric' # user to sign in as, defaults to "admin"
+PIN_PASSCODE_PIN = 1234        # the passcode required to login as the above user, using 0-9 and #
+```
+
+Add to `urls.py`:
+
+```python
+url(r'^admin/', include(admin.site.urls)),
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+try:
+    with open('README.md') as readme:
+        long_description = readme.read()
+except (IOError, ImportError):
+    long_description = ''
+
+
+setup(
+    install_requires=[
+        "selenium>=2.41.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "polished = polished.main:main"
+        ]
+    },
+    name="polished",
+    packages=find_packages(),
+    version="0.0.8",
+    author="Eric Carmichael",
+    author_email="eric@ckcollab.com",
+    description="Generates screenshots of a website based on git history",
+    long_description=long_description,
+    license="MIT",
+    keywords="link checker",
+    url="https://github.com/ckcollab/polished",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
