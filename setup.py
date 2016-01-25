5d2953a597fd20850203972bbfaaa977d97d125a
Eric Carmichael <eric@ckcollab.com>
2016-01-24 19:28:42 -0800

    woops forgot to bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="django-pin-passcode",
     packages=find_packages(),
     include_package_data=True, # declarations in MANIFEST.in
-    version="0.1.4",
+    version="0.1.5",
     author="Eric Carmichael",
     author_email="eric@ckcollab.com",
     description="A simple django app that provides site-wide easy password authentication for 1 user",
